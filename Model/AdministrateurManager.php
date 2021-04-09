b293e88dea225cd8d003bb4b2939bf74454b2842
DenisJongmanee <76998795+DenisJongmanee@users.noreply.github.com>
2021-04-09 14:03:25 +0200

    Changement du nom du dossier
    Nom du dossier : la_vallee_des_croisees

--- Model/AdministrateurManager.php
+++ Model/AdministrateurManager.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('camping/Model/Manager.php');
+require_once('la_vallee_des_croisees/Model/Manager.php');
 
 class AdministrateurManager extends Manager {
 
--- Model/Type_chaletManager.php
+++ Model/Type_chaletManager.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once('camping/Model/Manager.php');
+require_once('la_vallee_des_croisees/Model/Manager.php');
+
 
 class Type_chaletManager extends Manager {
 	

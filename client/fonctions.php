13fdeb1684a75ba0f00c52a9872081f78a26f4d2
DenisJongmanee <76998795+DenisJongmanee@users.noreply.github.com>
2021-04-12 16:38:48 +0200

    connexion

--- client/fonctions.php
+++ client/fonctions.php
@@ -1,7 +1,7 @@
 <?php
 
 function ajoutClient($nom,$prenom,$date_naissance,$mail,$telephone,$adresse,$cp_ville,$mdp_client,$ville) {
-		include('./connexionBDD_Denis.php');
+		include('../connexionBDD.php');
 		$sql = "INSERT INTO Client (nom, prenom, date_naissance, mail, telephone, adresse, cp_ville, mdp_client, ville) VALUES (?,?,?,?,?,?,?,?,?)";
 		$resultat = $conn->prepare($sql);
 		$resultat->execute(array($nom, $prenom, $date_naissance, $mail, $telephone, $adresse, $cp_ville, $mdp_client,$ville));
@@ -14,7 +14,7 @@ function ajoutClient($nom,$prenom,$date_naissance,$mail,$telephone,$adresse,$cp_
 	}
 
 function nonUniqueMail($mail) {
-	include('./connexionBDD_Denis.php');
+	include('../connexionBDD.php');
 	$sql = "SELECT * FROM Client WHERE mail=?";
 	$resultat=$conn->prepare($sql);
 	$resultat->execute(array($mail));
@@ -38,10 +38,10 @@ function verifAge($date) {
 }
 
 function ajoutReservation($idClient, $idChalet, $idSemaine) {
-	include('./connexionBDD_Denis.php');
-	$sql = "INSERT INTO Reservation (id_client, id_chalet, id_semaine, valide, date_reservation) VALUES (:id_client, :id_chalet, :id_semaine, :valide, :date_reservation)";
+	include('../connexionBDD.php');
+	$sql = "INSERT INTO Reservation (id_client, id_chalet, id_semaine, valide, date_reservation) VALUES (?,?,?,?,?)";
 	$resultat=$conn->prepare($sql);
-	$resultat->execute(array('idclient' => $idClient, 'id_chalet' => $idChalet, 'id_semaine' => $idSemaine, 'valide' => 'false', 'date_reservation' => date("Y-m-d")));
+	$resultat->execute(array($idClient, $idChalet, $idSemaine, 'false', date("Y-m-d")));
 	if ($resultat==FALSE) {
 		echo "probleme de la requete sql";
 	} else {
@@ -50,3 +50,26 @@ function ajoutReservation($idClient, $idChalet, $idSemaine) {
 }
 
 
+function Prix_total($idChalet) {
+	include('../connexionBDD.php');
+	$sql = "SELECT type_chalet.prix_base*Saison.taux+Prix_special.prix_modifie as prix_total FROM type_chalet INNER JOIN chalet ON type_chalet.id_type_chalet=chalet.id_type_chalet INNER JOIN Prix_special ON Chalet.id_chalet=Prix_special.id_chalet INNER JOIN Semaine ON Prix_special.id_semaine=Semaine.id_semaine INNER JOIN Saison ON Semaine.id_saison=Saison.id_saison WHERE id_chalet=?";
+	$resultat = $conn ->prepare($sql);
+	if ($resultat==FASE) {
+		echo "probleme de requete sql";
+	} else {
+		$donnee=$resultat->fetch();
+		return $donnee[0];
+	}
+}
+
+function listChaletRech($date_debut,$date_fin,$nbrePlace) {
+	include('../connexionBDD.php');
+	$sql = "SELECT chalet.id_chalet, type_chalet.libelle FROM type_chalet INNER JOIN chalet ON type_chalet.id_type_chalet=chalet.id_type_chalet INNER JOIN Prix_special ON Chalet.id_chalet=Prix_special.id_chalet INNER JOIN Semaine ON Prix_special.id_semaine=Semaine.id_semaine INNER JOIN Saison ON Semaine.id_saison=Saison.id_saison WHERE Semaine.date_debut>? AND Semaine.date_fin<? AND nb_place>?";
+	$resultat = $conn->prepare($sql);
+	$resultat 
+	if ($resultat==FALSE {
+		echo "probleme de requete sql";
+	} else {
+		return $resultat;
+	}
+}
